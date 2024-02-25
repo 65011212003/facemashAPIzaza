3e970392b5ac61c5c4cbc563ce939a8029b0d9a6
Chunpong Chan <122968364+65011212003@users.noreply.github.com>
2024-02-26 00:20:49 +0700

    123

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+import { NextFunction, Request, Response } from "express";
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, this is your simple Express TypeScript API!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
